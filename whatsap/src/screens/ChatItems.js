447a6fe3f3f1bb4a365ad5eea136dbf898129b3e
batuctn <batuctnn14@gmail.com>
2022-08-26 00:17:59 +0300

    Tıklandığında açılacak olan sohbetin içi sayfa eklenecek

---
+++ whatsap/src/screens/ChatItems.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ChatItems = () => {
+  return (
+    <View>
+      <Text>ChatItems</Text>
+    </View>
+  );
+};
+
+export default ChatItems;
