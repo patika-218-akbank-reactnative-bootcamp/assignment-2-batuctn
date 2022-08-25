d3ade1ae232ef9e3f4c18eea19995dcb1b961bc1
batuctn <batuctnn14@gmail.com>
2022-08-26 00:14:06 +0300

    Stack navigatör ile sayfalar birbirine bağlandı

--- whatsap/App.js
+++ whatsap/App.js
@@ -1,25 +1,20 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Icon from 'react-native-vector-icons/Ionicons';
-
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home sdsd</Text>
-      <Icon name="logo-firefox" size={30} color="#4F8EF7" />
-    </View>
-  );
-}
+import Chats from './src/screens/Chats';
+import ChatItems from './src/screens/ChatItems';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Chats" component={Chats} />
+        <Stack.Screen name="ChatItems" component={ChatItems} />
       </Stack.Navigator>
     </NavigationContainer>
   );
