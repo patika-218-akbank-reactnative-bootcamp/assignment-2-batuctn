0f493cc22d6c94b9701f9a28191e38450284fcb0
batuctn <batuctnn14@gmail.com>
2022-08-26 00:14:26 +0300

    mockdata-fakeapi eklendi.

---
+++ whatsap/src/services/api.js
@@ -0,0 +1,102 @@
+const chatData = [
+  {
+    id: '1',
+    receiver: {
+      id: '1',
+      firstName: 'Cem',
+      lastName: 'Eriş',
+      image: 'url',
+    },
+    messages: [
+      {
+        text: 'Naber',
+        datetime: '12:14',
+      },
+    ],
+  },
+  {
+    id: '2',
+    receiver: {
+      id: '1',
+      firstName: 'Kaan',
+      lastName: 'Can',
+    },
+    messages: [
+      {
+        text: 'Merada buluşalım',
+        datetime: '19:45',
+      },
+    ],
+  },
+  {
+    id: '3',
+    receiver: {
+      id: '1',
+      firstName: 'Kaan',
+      lastName: 'Can',
+    },
+    messages: [
+      {
+        text: 'Merada buluşalım',
+        datetime: '19:45',
+      },
+    ],
+  },
+  {
+    id: '4',
+    receiver: {
+      id: '1',
+      firstName: 'Kaan',
+      lastName: 'Can',
+    },
+    messages: [
+      {
+        text: 'Merada buluşalım',
+        datetime: '19:45',
+      },
+    ],
+  },
+  {
+    id: '5',
+    receiver: {
+      id: '1',
+      firstName: 'Kaan',
+      lastName: 'Can',
+    },
+    messages: [
+      {
+        text: 'Merada buluşalım',
+        datetime: '19:45',
+      },
+    ],
+  },
+  {
+    id: '6',
+    receiver: {
+      id: '1',
+      firstName: 'Kaan',
+      lastName: 'Can',
+    },
+    messages: [
+      {
+        text: 'Merada buluşalım',
+        datetime: '19:45',
+      },
+    ],
+  },
+  {
+    id: '7',
+    receiver: {
+      id: '1',
+      firstName: 'Kaan',
+      lastName: 'Can',
+    },
+    messages: [
+      {
+        text: 'Merada buluşalım',
+        datetime: '19:45',
+      },
+    ],
+  },
+];
+export default chatData;
