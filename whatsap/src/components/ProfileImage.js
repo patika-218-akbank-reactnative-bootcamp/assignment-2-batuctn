fe15506de0d72145e6258e1d0dc9f6bd054dbe38
batuctn <batuctnn14@gmail.com>
2022-08-26 17:11:37 +0300

    Profil image component created with props

--- whatsap/src/components/ProfileImage.js
+++ whatsap/src/components/ProfileImage.js
@@ -1,20 +1,22 @@
-import {View, Text} from 'react-native';
+import {View, Image, StyleSheet} from 'react-native';
 import React from 'react';
 
-const ProfileImage = ({image}) => {
+const ProfileImage = ({image, height, width}) => {
+  const inlineStyle = {
+    width: width,
+    height: height,
+  };
   return (
-    <View
-      style={{
-        backgroundColor: 'red',
-        height: 60,
-        width: 60,
-        borderRadius: 50,
-        margin: 8,
-        padding: 8,
-      }}>
-      {image}
+    <View style={[styles.image, inlineStyle]}>
+      <Image source={image} />
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  image: {
+    overflow: 'hidden',
+    borderRadius: 50,
+    margin: 10,
+  },
+});
 export default ProfileImage;
