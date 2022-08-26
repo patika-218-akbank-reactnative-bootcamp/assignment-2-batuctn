2949753b844a80ea65d11716a2f63e22b952865e
batuctn <batuctnn14@gmail.com>
2022-08-26 20:06:35 +0300

    mock data uptated

--- whatsap/src/services/api.js
+++ whatsap/src/services/api.js
@@ -44,5 +44,65 @@ const chatData = [
       },
     ],
   },
+  {
+    id: '4',
+    receiver: {
+      id: '1',
+      firstName: 'Yıldız',
+      lastName: 'Tilbe',
+      image: require('../assets/4.jpg'),
+    },
+    messages: [
+      {
+        text: 'Konserime davetlisin',
+        datetime: '21:45',
+      },
+    ],
+  },
+  {
+    id: '5',
+    receiver: {
+      id: '1',
+      firstName: 'Haluk',
+      lastName: 'Levent',
+      image: require('../assets/5.jpg'),
+    },
+    messages: [
+      {
+        text: 'Festival var gelirsen',
+        datetime: '00:31',
+      },
+    ],
+  },
+  {
+    id: '6',
+    receiver: {
+      id: '1',
+      firstName: 'Teoman',
+      lastName: 'Yakupoğlu',
+      image: require('../assets/6.jpg'),
+    },
+    messages: [
+      {
+        text: 'Müzik bırakılır mı ya?',
+        datetime: '23:11',
+      },
+    ],
+  },
+  {
+    id: '7',
+    receiver: {
+      id: '1',
+      firstName: 'Melek',
+      lastName: 'Mosso',
+      image: require('../assets/7.jpg'),
+    },
+    messages: [
+      {
+        text: 'Yarın akşam yemeğe gidiyoruz haberin olsun',
+        datetime: '01:45',
+      },
+    ],
+  },
 ];
 export default chatData;
