dd76375b34887b02ef4d8d0ecf109089c44eee94
batuctn <batuctnn14@gmail.com>
2022-08-26 17:10:40 +0300

    FlatList add,param navigatör add,stylesheet kullanmı eklendi.

--- whatsap/src/components/ChatList.js
+++ whatsap/src/components/ChatList.js
@@ -1,38 +1,69 @@
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
 import chatData from '../services/api';
 import ProfileImage from './ProfileImage';
+import {useNavigation} from '@react-navigation/native';
 
 const ChatList = () => {
+  const {navigate} = useNavigation();
   const renderUser = ({item, index}) => (
-    <View style={{flexDirection: 'row'}}>
-      <View>
-        <ProfileImage />
-      </View>
-      <View style={{width: '80%', borderBottomWidth: 1}}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            marginRight: 10,
-          }}>
-          <Text
-            style={{
-              marginLeft: 5,
-              fontSize: 16,
-              fontWeight: '500',
-              color: 'black',
-              padding: 3,
-            }}>
-            {`${item.receiver.firstName} ${item.receiver.lastName}`}
-          </Text>
-          <Text style={{padding: 3}}>{item.messages[0].datetime}</Text>
+    <TouchableOpacity
+      onPress={() => {
+        navigate('ChatItems', {
+          firstName: item.receiver.firstName,
+          lastName: item.receiver.lastName,
+          messages: item.messages[0].text,
+          datetime: item.messages[0].datetime,
+        });
+      }}>
+      <View style={styles.contain}>
+        <View>
+          <ProfileImage image={item.receiver.image} height={60} width={60} />
         </View>
-        <Text style={{marginLeft: 5, padding: 2}}>{item.messages[0].text}</Text>
+        <View style={styles.chatScreen}>
+          <View style={styles.fullNameTextView}>
+            <Text style={styles.fullNameText}>
+              {`${item.receiver.firstName} ${item.receiver.lastName}`}
+            </Text>
+            <Text style={styles.padding3}>{item.messages[0].datetime}</Text>
+          </View>
+          <Text style={styles.messages}>{item.messages[0].text}</Text>
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
-  return <FlatList data={chatData} renderItem={renderUser} />;
+  return (
+    <FlatList nestedScrollEnabled data={chatData} renderItem={renderUser} />
+  );
 };
+
+const styles = StyleSheet.create({
+  contain: {
+    flexDirection: 'row',
+  },
+  chatScreen: {
+    width: '80%',
+    borderBottomWidth: 0.5,
+  },
+  fullNameTextView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginRight: 10,
+  },
+  fullNameText: {
+    marginLeft: 5,
+    fontSize: 16,
+    fontWeight: '500',
+    color: 'black',
+    padding: 3,
+  },
+  padding3: {
+    padding: 3,
+  },
+  messages: {
+    marginLeft: 5,
+    padding: 2,
+  },
+});
 
 export default ChatList;
